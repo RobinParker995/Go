a6af5f2218f231da15f3f86f1a1f3634704edd3d
RobinParker995 <robinparker995@gmail.com>
2026-05-19 19:52:55 +0530

    Made some changes

--- Variable/main.go
+++ Variable/main.go
@@ -8,36 +8,52 @@ func main(){
 	name := "Robin"
 	age := 18
 
+    fmt.Println("Shorthand Versions: ")
 	fmt.Println(name)
 	fmt.Println(age)
+
+    fmt.Printf("\n")
 
 	//Explicit Version
 	var commission float64  = 18.50
 	var isAdult bool = true
 
+    fmt.Println("Explict Versions: ")
 	fmt.Printf("%f \n", commission);
 	fmt.Printf("%t \n", isAdult);	
 
+    fmt.Printf("\n")
+
 	//Rune ane Bytes
 	var b byte = 65
+
+    fmt.Println("Bytes: ")
 	fmt.Println(b)
 	fmt.Printf("%c\n", b)
 
+    fmt.Printf("\n")
 	
 	var r rune = '🔥'
+
+    fmt.Println("Runes: ")
 	fmt.Println(r)
 	fmt.Printf("%c\n", r)
 
+    fmt.Printf("\n")
 
-	//Iterating By Bytes
+
+    //Iteration
 	s := "😎"
 
+
+    fmt.Println("Iteration by Bytes: ")
 	for i := 0; i < len(s); i++ {
     		fmt.Println(s[i])
 	}
 
+    fmt.Printf("\n")
 
-	//Iterating by Runes
+	fmt.Println("Iterating by Runes")
 	for _, r := range s {
 	    fmt.Printf("%c\n", r)
 	}
