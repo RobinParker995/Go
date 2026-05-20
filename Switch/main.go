ac9cc5082ee6712e9e4695b857efbf6b1e4159b9
RobinParker995 <robinparker995@gmail.com>
2026-05-20 12:21:06 +0530

    Updated Switch Code

--- Switch/main.go
+++ Switch/main.go
@@ -20,4 +20,68 @@ func main() {
 		fmt.Println("Invalid")
 	}
 
+    fmt.Printf("\n")
+
+    //Multiple Value in one case
+
+    grade := "B"
+
+    switch grade {
+
+    case "A", "B":
+         fmt.Println("Good")
+
+    case "C":
+         fmt.Println("Average")
+
+    default:
+        fmt.Println("Needs Improvement")
+}
+
+    fmt.Printf("\n")
+
+
+    //Switch Without Expression
+
+    age := 14
+
+    switch {
+
+    case age >= 18:
+         fmt.Println("Adult")
+
+    case age >= 13:
+         fmt.Println("Teenager")
+
+    default:
+        fmt.Println("Child")
+}
+
+    fmt.Printf("\n")
+
+    //Fallthrough - Continuing the statement even after the case is met
+
+    role := "editor"
+
+    fmt.Printf("%s can: \n", role)
+
+    switch role {
+
+    case "admin":
+         fmt.Println("Can delete Users")
+         fallthrough
+
+    case "editor":
+         fmt.Println("Can Edit Content")
+         fallthrough
+
+    case "user":
+         fmt.Println("Can view Content")
+
+    default:
+        fmt.Println("Invalid Role")
+
+}
+
+
 }
