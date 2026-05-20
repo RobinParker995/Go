259399d9bfd4c6dc84a745637d1b057b37474d0c
RobinParker995 <robinparker995@gmail.com>
2026-05-20 11:56:54 +0530

    Added Switch

---
+++ Switch/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Basic Syntax
+
+	option := 2
+
+	switch option {
+
+	case 1:
+		fmt.Println("English")
+
+	case 2:
+		fmt.Println("Spanish")
+
+	default:
+		fmt.Println("Invalid")
+	}
+
+}
